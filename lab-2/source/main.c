41b4821bd1d2050a5d562b7d82fc8adb57374318
gruvw <gruvw.dev@gmail.com>
2023-09-23 23:21:55 +0200

    Finished LAB 2

--- lab-2/source/main.c
+++ lab-2/source/main.c
@@ -6,11 +6,44 @@
 #include <stdio.h>
 
 int main(void) {
-
     consoleDemoInit();
-    printf("\nI love you Sarah ! <3");
 
-    while(1){
+    /* Exercise 1 */
+
+    printf("\nHello this is me Mario");
+
+    /* Exercise 2 */
+
+    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    int min = array[0];  // error if array is empty as there is no min/max
+    int max = array[0];
+    int sum = 0;
+    int length = sizeof(array) / sizeof(*array);
+
+    for (size_t i = 0; i < length; i++) {
+        min = array[i] < min ? array[i] : min;
+        max = array[i] > max ? array[i] : max;
+        sum += array[i];
+    }
+
+    int average = sum / length;
+
+    printf("\nMin: %d, Max: %d, Average: %d\n\n", min, max, average);
+
+    /* Exercise 3 */
+
+    const int SIZE = 3;
+    int matrix[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (size_t i = 0; i < SIZE; i++) {
+        for (size_t j = 0; j < SIZE; j++) {
+            printf("%d ", matrix[i * SIZE + j]);
+        }
+        printf("\n\n");
+    }
+
+    while (1) {
         swiWaitForVBlank();
-}
+    }
 }
