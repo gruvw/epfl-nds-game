a4b2dfdbe96c1a523954ff7071d5d2ca224d98c4
gruvw <gruvw.dev@gmail.com>
2023-09-24 14:27:36 +0200

    Exercises 4 + 5

--- lab-2/source/main.c
+++ lab-2/source/main.c
@@ -1,12 +1,46 @@
 #include <nds.h>
 #include <stdio.h>
 
+/* Exercise 4 */
+
+int factorial_iter(int n) {
+    int res = 1;
+
+    for (int i = n; i >= 2; i--) {
+        res *= i;
+    }
+
+    return res;
+}
+
+int factorial_rec(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+
+    return n * factorial_rec(n - 1);
+}
+
+/* Exercise 5 */
+
+int gcd(int a, int b) {
+    int rem = a % b;
+
+    if (!rem) {
+        return b;
+    }
+
+    return gcd(b, rem);
+}
+
+/* MAIN */
+
 int main(void) {
     consoleDemoInit();
 
     /* Exercise 1 */
 
-    printf("\nHello this is me Mario");
+    printf("\nHello this is me Mario\n");
 
     /* Exercise 2 */
 
@@ -38,6 +72,16 @@ int main(void) {
         }
         printf("\n\n");
     }
+
+    /* Exercise 4 */
+
+    const int NB = 4;
+    printf("Factorial of %d: iter %d, rec %d\n", NB, factorial_iter(NB), factorial_rec(NB));
+
+    /* Exercise 5 */
+
+    const int A = 20, B = 15;
+    printf("\nGCD of %d & %d: %d", A, B, gcd(A, B));
 
     while (1) {
         swiWaitForVBlank();
