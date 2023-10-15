1bc99815289dd1905172ea05c84bb53a4d072379
gruvw <gruvw.dev@gmail.com>
2023-10-15 21:48:40 +0200

    lab-4 sols

--- lab-4/source/addition.s
+++ lab-4/source/addition.s
@@ -1,12 +1,26 @@
-.ARM
-.ALIGN
-.GLOBL Sum
-.TYPE Sum,function
 
-.text
+@------------------------------------------------------------------------------
+@--------------------------------------Sum-------------------------------------
+@------------------------------------------------------------------------------
+
+.ARM
+
+.ALIGN
+
+.GLOBL  Sum
+
+.TYPE   Sum,function   @ Not always required but useful for .map files
+
 Sum:
-    stmdb sp!, {r4 - r11, lr}
-    add r0, r1
-    ldmia sp!, {r4 - r11, lr}
-    bx lr
-.end
+
+	stmdb sp!, {r4 - r11, lr} @ instruction to push an entire specified set of
+							  @ register onto the stack in one instruction
+
+
+	add r0, r1 				  @ r0=r0 + r1; r0 is used also for the return value
+
+
+
+	ldmia sp!, {r4 - r11, lr} @ instruction to restore the previous saved registers
+	bx lr 					  @ jump to the return address
+
