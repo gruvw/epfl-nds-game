c2aeb1dbd3c36042262280db5611ee777ed852aa
gruvw <gruvw.dev@gmail.com>
2023-11-18 18:52:46 +0100

    Lab 7

--- lab-7/source/P_Map16x16.c
+++ lab-7/source/P_Map16x16.c
@@ -1,13 +1,13 @@
 
 #include "P_Map16x16.h"
+
 #include "controls.h"
 
-u16* mapMemory;
+u16 * mapMemory;
 int MapCols;
 int MapRows;
 
-void P_Map16x16_configureBG2()
-{
+void P_Map16x16_configureBG2() {
     //Initialize Background
     BGCTRL[2] = BG_MAP_BASE(0) | BgSize_B16_256x256;
 
@@ -22,9 +22,8 @@ void P_Map16x16_configureBG2()
     mapMemory = BG_GFX;
 }
 
-void P_Map16x16_configureBG2_Sub()
-{
-	//Configure BG 2 Sub. (Do not forget the BG_BMP_BASE configuration)
+void P_Map16x16_configureBG2_Sub() {
+    //Configure BG 2 Sub. (Do not forget the BG_BMP_BASE configuration)
     BGCTRL_SUB[2] = BG_BMP_BASE(0) | BgSize_B8_256x256;
 
     //Affine Marix Transformation
@@ -33,24 +32,19 @@ void P_Map16x16_configureBG2_Sub()
     REG_BG2PB_SUB = 0;
     REG_BG2PD_SUB = 256;
 
-    swiCopy(controlsPal, BG_PALETTE_SUB, controlsPalLen/2);
-    swiCopy(controlsBitmap, BG_GFX_SUB, controlsBitmapLen/2);
+    swiCopy(controlsPal, BG_PALETTE_SUB, controlsPalLen / 2);
+    swiCopy(controlsBitmap, BG_GFX_SUB, controlsBitmapLen / 2);
 }
 
-void P_Map16x16_configureBG0()
-{
-
+void P_Map16x16_configureBG0() {
 }
 
-void P_Map16x16_configureBG3()
-{
-
+void P_Map16x16_configureBG3() {
 }
 
-void P_Map16x16_Init( int cols, int rows )
-{
-    MapCols=cols;
-    MapRows=rows;
+void P_Map16x16_Init(int cols, int rows) {
+    MapCols = cols;
+    MapRows = rows;
 #ifdef ROTOSCALE
     //Configure BG2
     P_Map16x16_configureBG2();
@@ -59,27 +53,23 @@ void P_Map16x16_Init( int cols, int rows )
 #endif
 }
 
-
- void SetMap16x16To(int index16, bool full)
- {
-	    //switch x and y
-	    int x = index16 % MapCols;
-	    int y = index16 / MapCols;
-	    //now inverse x
-	    y = -(y-MapRows+1);
-	    index16 = x * MapRows + y;
+void SetMap16x16To(int index16, bool full) {
+    //switch x and y
+    int x = index16 % MapCols;
+    int y = index16 / MapCols;
+    //now inverse x
+    y = -(y - MapRows + 1);
+    index16 = x * MapRows + y;
 #ifdef FB0
-	    u16 value = full ? RGB15(31,0,0) : RGB15(0,0,0);
-	    FillRectangle(MAIN,x*16, x*16+16, y*16, y*16+16,value);
+    u16 value = full ? RGB15(31, 0, 0) : RGB15(0, 0, 0);
+    FillRectangle(MAIN, x * 16, x * 16 + 16, y * 16, y * 16 + 16, value);
 #endif
 
 #ifdef ROTOSCALE
-     
-     if (full==1)
-         FillRectangle(MAIN,x*16, x*16+16, y*16, y*16+16,ARGB16(1,31,0,0));
-     else
-         FillRectangle(MAIN,x*16, x*16+16, y*16, y*16+16,ARGB16(1,0,0,0));
+
+    if (full == 1)
+        FillRectangle(MAIN, x * 16, x * 16 + 16, y * 16, y * 16 + 16, ARGB16(1, 31, 0, 0));
+    else
+        FillRectangle(MAIN, x * 16, x * 16 + 16, y * 16, y * 16 + 16, ARGB16(1, 0, 0, 0));
 #endif
-
 }
-
