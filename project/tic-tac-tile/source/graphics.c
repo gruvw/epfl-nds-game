84418711205efb2d221db23c1a7563ae6588caf5
gruvw <gruvw.dev@gmail.com>
2023-12-09 17:12:53 +0100

    Working on NDS (fixed)

--- project/tic-tac-tile/source/graphics.c
+++ project/tic-tac-tile/source/graphics.c
@@ -2,25 +2,29 @@
 #include <stddef.h>
 #include <string.h>
 
-#include "nds/arm9/background.h"
-#include "nds/arm9/video.h"
-#include "nds/ndstypes.h"
-
 #include "a-palette.h"
 #include "b-background.h"
 #include "c-cross.h"
 #include "d-circle.h"
 #include "e-select.h"
+#include "nds/arm9/background.h"
+#include "nds/arm9/video.h"
+#include "nds/ndstypes.h"
 
-#define CROSS_PALETTE_INCR 2
-#define CIRCLE_PALETTE_INCR 3
-#define SELECT_PALETTE_INCR 4
+#define BG_PALETTE_INCR 1
+#define CROSS_PALETTE_INCR 3
+#define CIRCLE_PALETTE_INCR 4
+#define SELECT_PALETTE_INCR 5
 
 // === Utilities ===
 
 void set_bg_transform(size_t bg_nb) {
-    bgTransform[bg_nb]->vdy = bgTransform[bg_nb]->hdx = BIT(8);
-    bgTransform[bg_nb]->dy = bgTransform[bg_nb]->dx = bgTransform[bg_nb]->hdy = bgTransform[bg_nb]->vdx = 0;
+	bgTransform[bg_nb]->hdx = BIT(8);
+	bgTransform[bg_nb]->vdx = 0*256;
+	bgTransform[bg_nb]->hdy = 0*256;
+	bgTransform[bg_nb]->vdy = BIT(8);
+	bgTransform[bg_nb]->dx = 0*256;
+	bgTransform[bg_nb]->dy = 0*256;
 }
 
 // === Images Palette Corrections ===
@@ -33,6 +37,7 @@ void correct_palette(void * data, size_t len, int increment) {
 }
 
 void images_palette_correction() {
+    correct_palette((void *) b_backgroundBitmap, b_backgroundBitmapLen, BG_PALETTE_INCR);
     correct_palette((void *) c_crossBitmap, c_crossBitmapLen, CROSS_PALETTE_INCR);
     correct_palette((void *) d_circleBitmap, d_circleBitmapLen, CIRCLE_PALETTE_INCR);
     correct_palette((void *) e_selectBitmap, e_selectBitmapLen, SELECT_PALETTE_INCR);
@@ -41,19 +46,31 @@ void images_palette_correction() {
 // === Setup ===
 
 void graphics_setup() {
-    VRAM_A_CR = VRAM_ENABLE | VRAM_A_MAIN_BG;
+    // Graphics power
+    REG_POWERCNT |= POWER_LCD | POWER_2D_A | POWER_2D_B;
 
+    // Graphics setup control registers
+    VRAM_A_CR = VRAM_ENABLE | VRAM_A_MAIN_BG;
     REG_DISPCNT = MODE_5_2D | DISPLAY_BG2_ACTIVE | DISPLAY_BG3_ACTIVE;
 
     BGCTRL[2] = BG_BMP_BASE(0) | BgSize_B8_256x256;
     BGCTRL[3] = BG_BMP_BASE(3) | BgSize_B8_256x256;
 
-    swiCopy(a_palettePal, BG_PALETTE, a_palettePalLen / 2);
+    set_bg_transform(2);
+    set_bg_transform(3);
+
+    // Reset all backgrounds
+    memset(BG_BMP_RAM(0), 0, SCREEN_WIDTH * SCREEN_HEIGHT);
+    memset(BG_BMP_RAM(3), 0, SCREEN_WIDTH * SCREEN_HEIGHT);
+
+    // Prepare palettes
+    const u16 * pal = a_paletteBitmap;
+    for (size_t i = 0; i < 128; i++) {
+        BG_PALETTE[i + 1] = pal[i];
+    }
 
     images_palette_correction();
 
-    set_bg_transform(2);
-    set_bg_transform(3);
 }
 
 // === Graphics Drawing ===
@@ -62,11 +79,11 @@ void set_background() {
     swiCopy(b_backgroundBitmap, BG_BMP_RAM(3), b_backgroundBitmapLen / 2);
 }
 
-void overlay_sprite(void * sprite, size_t row_pos, size_t col_pos, size_t side) {
-    const u16 * s = sprite;
+void overlay_sprite(const u16 * sprite, size_t row_pos, size_t col_pos, size_t side) {
     for (size_t col = 0; col < side / 2; col++) {
         for (size_t row = 0; row < side; row++) {
-            BG_BMP_RAM(0)[(row_pos + row) * SCREEN_WIDTH / 2 + col_pos / 2 + col] = s[row * side / 2 + col];
+            size_t pos = (row_pos + row) * SCREEN_WIDTH / 2 + col_pos / 2 + col;
+            BG_BMP_RAM(0)[pos] = sprite[row * side / 2 + col];
         }
     }
 }
--- project/tic-tac-tile/source/graphics.h
+++ project/tic-tac-tile/source/graphics.h
@@ -1,7 +1,8 @@
 #include <stddef.h>
+#include "nds/ndstypes.h"
 
 void graphics_setup();
 void set_background();
-void overlay_sprite(void * sprite, size_t row_pos, size_t col_pos, size_t side);
+void overlay_sprite(const u16 * sprite, size_t row_pos, size_t col_pos, size_t side);
 
 void main_graphics();
