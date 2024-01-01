6d09c197266094947b9fe58dac5ea19edfcc1f3d
gruvw <gruvw.dev@gmail.com>
2024-01-01 16:16:02 +0100

    Prepared Wi-Fi

---
+++ project/tic-tac-tile/source/controller/wifi/WiFi_minilib.h
@@ -0,0 +1,30 @@
+#ifndef WIFI_MINILIB_H
+#define WIFI_MINILIB_H
+
+#include <nds.h>
+#include <dswifi9.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define SSID "SEM-NDS"
+
+// WiFi initialization
+int initWiFi();
+
+// Socket initialization
+int openSocket();
+
+// Receive data trough the socket
+int receiveData(char * data_buff, int bytes);
+
+// Send data through the socket
+int sendData(char * data_buff, int bytes);
+
+// Close the Socket
+void closeSocket();
+
+// Disconnect the WiFi
+void disconnectFromWiFi();
+
+#endif
